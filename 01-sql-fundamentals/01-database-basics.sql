00798e6efdecdf89f94127d60823df78aa8385a5
Dennis  Chepkwony <denohkiplangat2025@gmail.com>
2026-07-29 00:33:48 +0300

    feat: add lesson 1 SQL queries

--- 01-sql-fundamentals/01-database-basics.sql
+++ 01-sql-fundamentals/01-database-basics.sql
@@ -1,2 +1,24 @@
 SELECT*
 FROM customers
+
+-- Retrieve customers from Germany filtering rows together with columns
+SELECT
+first_name,
+country
+FROM customers
+WHERE country = 'Germany'
+
+-- Retrieve all customers and sort the results by the highest score first.
+SELECT*
+FROM customers
+ORDER BY score DESC
+
+-- Retrieve all customers and sort the results by the lowest score first.
+SELECT*
+FROM customers
+ORDER BY score ASC
+
+-- Retrieve all customers and sort the results by the country and then by the highest score
+SELECT*
+FROM customers
+ORDER BY country ASC,score DESC
